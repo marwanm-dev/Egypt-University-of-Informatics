5ee89774a79f1568dbedc413d770d02096460749
marwanm-dev <marwanm.dev@gmail.com>
2024-01-28 11:40:39 +0200

    feat: add logic for the best computer turn's move based on difficulty

--- Bingo Game/main.py
+++ Bingo Game/main.py
@@ -157,7 +157,80 @@ def play_game(name, difficulty, crossed_dict, user_board, comp_board):
         handle_turn(chosen_number, False)
 
     def make_comp_move():
-        pass
+        def random_percentage(percent): 
+            perc = float(percent)
+            num = float(randint(0, 100))
+            return num <= perc
+
+        def count_crossed_in_row(row_index):
+            return sum(1 for pos in crossed_dict.keys() if pos[1] == row_index)
+
+        def count_crossed_in_column(col_index):
+            return sum(1 for pos in crossed_dict.keys() if pos[0] == col_index)
+
+        def count_crossed_in_diagonal(is_main):
+            return sum(1 for pos in crossed_dict.keys() if (pos[0] == pos[1] if is_main else pos[0] + pos[1] == dim - 1))
+
+        chosen_numbers = []
+
+        initiate_smart_move = random_percentage(difficulty)
+
+        if not initiate_smart_move: 
+            chosen_number = randint(1, dim**2 + 1)
+            while chosen_number in crossed_dict.values():
+                chosen_number = randint(1, dim**2 + 1)
+        else:
+            priority_map = [] # three pair tuples, each consisting of 1st: the maximum count of the crossed spots in the row, column, and diagonal (acting as the priority) and 2nd: the avaialble numbers under that index
+
+            # Count crossed numbers in each row
+            list_count_row = []
+            for i in range(dim):
+                count_r = count_crossed_in_row(i)
+                list_count_row.append(count_r if count_r < dim else -1) # a flag for indicating its filled (equals the dim) so it gets filtered out by the max function due to being the lowest integer
+            max_count_row_i = list_count_row.index(max(list_count_row)) # If multiple items are maximal, the function returns the first one encountered. - https://docs.python.org/3/library/functions.html#max
+            available_spots_row = [elem for elem in comp_board[max_count_row_i] if elem != "X"]
+            priority_map.append((max(list_count_row), available_spots_row))
+
+            # Count crossed numbers in each column
+            list_count_col = []
+            for i in range(dim):
+                count_c = count_crossed_in_column(i)
+                list_count_col.append(count_c if count_c < dim else -1)
+            max_count_column_i = list_count_col.index(max(list_count_col))
+            available_spots_col = [row[max_count_column_i] for row in comp_board if row[max_count_column_i] != "X"]
+            priority_map.append((max(list_count_col), available_spots_col))
+
+            # Count crossed numbers in both diagonal
+            count_main__d = count_crossed_in_diagonal(True)
+            count_sec__d = count_crossed_in_diagonal(False)
+            available_spots_main_d = [comp_board[i][i] for i in range(dim) if comp_board[i][i] != "X"]
+            available_spots_sec_d = [comp_board[i][dim - i - 1] for i in range(dim) if comp_board[i][dim - i - 1] != "X"]
+
+            if count_main__d == count_sec__d:
+                if count_main__d != dim: # Both are not crossed entirely
+                    priority_map.append((count_main__d, available_spots_main_d + available_spots_sec_d))
+            if count_main__d > count_sec__d: # if the main diagonal have more crossed
+                if count_main__d != dim: # if the main diagonal isn't all crossed
+                    priority_map.append((count_main__d, available_spots_main_d))
+                else: priority_map.append((count_sec__d, available_spots_sec_d)) # since the main diagonal can't be bigger and equal to the secondary diagonal at the same time, it must have available spots in the secondary one
+            elif count_sec__d > count_main__d: # if the main diagonal have more crossed
+                if count_main__d != dim: # if the secondary diagonal isn't all crossed
+                    priority_map.append((count_sec__d, available_spots_sec_d))
+                priority_map.append((count_main__d, available_spots_main_d)) # since the secondary diagonal can't be bigger and equal to the main diagonal at the same time, it must have available spots in the main diagonal
+            
+            # Getting the best option out of the three (each one of the three is the best option in its type, row, column, or diagonal)
+            max_priority = -1
+            chosen_numbers = [] # the best set of numbers (with same priorities) to choose from
+
+            for priority, numbers in priority_map:
+                if priority != dim:
+                    if priority > max_priority:
+                        max_priority = priority
+                        chosen_numbers = numbers
+                    elif priority == max_priority: chosen_numbers += numbers # without this, it'll be choosing the occurrence of the first priority which is in the rows due to its first position only resulting in prioritizing the rows over others columns, and columns over diagonals.
+            chosen_number = choice(chosen_numbers) 
+
+        handle_turn(chosen_number, True)
 
     if not dim: return True
 
