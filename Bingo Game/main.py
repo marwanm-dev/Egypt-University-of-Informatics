06dd9d52f8c5c1ca8e2e9dd1205fff884da2d2af
marwanm-dev <marwanm.dev@gmail.com>
2024-01-28 11:36:23 +0200

    feat: save progress

--- Bingo Game/main.py
+++ Bingo Game/main.py
@@ -1,179 +1,258 @@
-from random import randint, sample
-from utility import init_arr, generateRandomNumber, wait
+from random import sample
+from utility import init_arr, get_random_message
 
 # Global Variables
 dim=None
-state=None # 0 for loss, 1 for win
-randomMessages = {"randomNumberScene": ["Where's that number on my board..", "I can't find it..", "let me see.."],
-                    "endGameScene":["Thank you for playing! See you as soon as possible", "It was a good game, GG", "That was easy.. See you soon"]}
+user_won=None
+user_score=0
+comp_score=0
+user_total_score=0
+comp_total_score=0
 
 # Functions
-def handleReset(name):
-    global dim, state
-
-    # if state == 0:
-    #     message = "Lose again "
-    # else:
-    #     message = "Give me another chance "
-    message = "Lose again " if state == 0 else "Give me another chance " # Please no 0.5/5
-    ans = input(f"{message}or quit （　＾ω＾）? (y/n)")
-
-    while ans not in ("y", "n"):
-        ans = input("Invalid input. Enter 'y' or 'n' :) ")
-    if ans == "y":
-        dim = None
-        state = None
-        main(name)
-    else: bye(name)
-
 def welcome(name):
-    print(f"\nWelcome {name}!", end=f"\n")
-    return name
+    print(f"\n{get_random_message('welcome_scene')} {name}!\n")
 
-def bye(name):
-    print(f"\n{randomMessages['endGameScene'][randint(0, len(randomMessages['endGameScene']) - 1)]} {name}\n")
-    
-def handle_dims():
-    ans = input("Do you want to continue with 5x5 board? (y/n/q) ")
-    while ans not in ("y", "n", "q"):
-        ans = input("Invalid input. Enter 'y', 'n', or 'q'. ")
-    # if ans == "y":
-    #     return 5
-    # elif ans == "n":
-    #     return int(input("Enter your dimension: "))
-    # return dim # which is None
-    return 5 if ans == "y" else int(input("Enter your dimension: ")) if ans == "n" else dim # which is None
+def bye():
+    print(f"{get_random_message('bye_scene')}\n")
 
-def init(dim):
+def get_dimension():
+    global dim
+
+    user_input = input("Do you want to continue with 5x5 board? (y/n/q) ")
+    while user_input not in ("y", "n", "q"):
+        user_input = input("Invalid input. Enter 'y', 'n', or 'q'. ")
+
+    if user_input == "y": dim = 5
+    elif user_input == "n":
+        ans = input("Enter your dimension: ")
+        while not ans.isdigit() or int(ans) < 5: # must be in that order or if a non-digit entered will yield an error for converting to int
+            ans = input("Invalid dimension. Must be a digit and more than or equal to 5.\nPlease enter again: ")
+        dim = int(ans)
+    else: # indicating the exit (user pressed "q")
+        bye()
+        return [], []
+    return init_board(dim)
+
+def init_board(dim):
     user_board = init_arr(dim)
     comp_board = init_arr(dim)
-
     create_boards(user_board, comp_board)
-
-    return user_board, comp_board # Multiple returns, acts as a tuple
+    return user_board, comp_board
 
 def create_boards(user_board, comp_board):
     def create_board(board):
         randomized = sample(range(1, dim**2 + 1), dim**2)
         curr_row = 0
-        # appending each number in its correct position by shifting the ones after the required row length to the other nested list
+
         for i in randomized:
+            # appending each number in its correct position by shifting the ones after the required row length to the other nested list
             board[curr_row].append(i)
             if len(board[curr_row]) == dim: curr_row += 1
 
     create_board(user_board)
     create_board(comp_board)
 
-def check_board(board, userTurn):
-    def change_state():
-        global state
-
-        # # if userTurn == True: state = 1
-        # if userTurn: state = 1 
-        # else: state = 0
-        state = 1 if userTurn else 0
+def check_board(board):
+    score=0
 
     # Row check
     for i in board:
         for j in i:
             if j != "X": break
-        else: change_state()
+        else: score+=1
 
     # Column check
-    for i in range(0, dim):
+    for i in range(dim):
         for j in board:
             if j[i] != "X": break
-        else: change_state()
+        else: score+=1
     
     # Diagonal check
+
+    # checks if leftmost/rightmost variable bound is indeed an X, then continues the loop without breaking, so if ended successfully (enters else block) score+=1
     for j in board:
-        if j[board.index(j)] != "X" and j[len(j) - 1 - board.index(j)] != "X": break # checking if leftmost variable bound isn't an X and if rightmost variable bound isn't an X also, and that makes it impossible to fill the diagonal anyway, resulting in a break out of loop
-    else: change_state() # will run if the loop continues without breakage
-    
-    return state
+        if j[board.index(j)] == "X": continue
+        break
+    else: score+=1
+
+    for j in board:
+        if j[len(j) - 1 - board.index(j)] == "X": continue
+        break
+    else: score+=1
+
+    return score
 
 def cross_board(board, randomNumber):
-    for i in range(0, dim):
+    for i in range(dim):
         if randomNumber in board[i]:
             board[i][board[i].index(randomNumber)] = "X"
 
-    return state
-
-def print_boards(user_board, comp_board, isPlaying = False):
+def print_boards(user_board, comp_board, inGame = False):
     def print_board(board, name):
+        WIDTH=5
+        sep=f"{'-' * (dim * (WIDTH + 1) - 2)}" # as dynamic as I possibly can make
         print(sep, f"{name:^{len(sep)}}", sep, sep="\n")
         for row in board:
             for num in row:
                 print(f"{num:^{WIDTH}}", end=" ")
             print("\n")
-    WIDTH=5
-    sep=f"{'-' * (dim * (WIDTH + 1) - 2)}" # as dynamic as I possibly can make
 
-    if isPlaying == True:
+    if inGame == True:
         print_board(user_board, "Your Turn")
-        print(randomMessages["randomNumberScene"][randint(0, len(randomMessages["randomNumberScene"]) - 1)], end="\n")
-        wait()
-        print("\n")
-        if state == 1:
-            print("Wait a minute.. ")
-            print("I already lost :O \n")
-            print_board(comp_board, "My board")
-        else: print_board(comp_board,"My Turn")
+        print(f"{get_random_message('game_scene')}\n")
+        if user_won:
+            print("Wait a sec.. You already beat me :(\n")
+            print_board(comp_board, "Computer Board")
+        else: print_board(comp_board,"Computer Turn")
     else:
         print_board(user_board, "Your Board")
-        print_board(comp_board, "My Board")
+        print_board(comp_board, "Computer Board")
 
-def play(user_board, comp_board, name):
-    def handleInput(message):
-        key = input(message)
-        while key != "":
-            key = input("It's literally labeled 'Enter', not on right side of your non-mechanical-membrane keyboard :P ")
+def handle_number_validation(dim, crossed, name, user_board, comp_board):
+    def handle_menu():
+        choice = input("1. Save and Quit\n2. Quit without saving\n3. Save and Continue\n4. Continue without Saving\n- ")
+        while choice not in ("1", "2", "3", "4"):
+            choice = input("Invalid input. Enter '1', '2', '3', or '4' :) ")
 
-    def handleTurns(user_board, comp_board):
-        cross_board(user_board, randomNumber)
-        check_board(user_board, True)
+        if choice in ("1", "3"): save_game(name, user_board, comp_board)
+        if choice not in ("3", "4"): return 0
+        else: return handle_number_validation(dim, crossed, name, user_board, comp_board)
 
-        if state == 1: return # Don't switch to computer's turn if user already won 
+    while True:
+        user_input = input("Enter a number to cross or open menu (M/m): ")
+        if user_input.lower() == 'm':
+            return handle_menu()
+        elif user_input.isdigit():
+            num = int(user_input)
+            if num in range(1, dim**2 + 1) and num not in crossed:
+                crossed.append(num)
+                return num
+            else:
+                print("Invalid input. Already crossed or out of range.\n")
+        else:
+            print("Invalid input. Must be a number or a key-binding (M/m)\n")
 
-        cross_board(comp_board, randomNumber)
-        check_board(comp_board, False)
+def play_game(name, user_board, comp_board):
+    BINGO = "BINGO"
+    PENDING_WORD = "_____"
+    
+    def check_win():
+        global user_total_score, comp_total_score, user_won
+        if user_score == len(BINGO):
+            user_total_score+=1
+            user_won=True
+        if comp_score == len(BINGO): # not "elif"
+            comp_total_score+=1
+            user_won=False
+
+    def handleTurns():
+        global user_score, comp_score
+        
+        cross_board(user_board, chosen_number)
+        user_score = check_board(user_board)
+        check_win()
+
+        if user_won: return # Don't switch to computer's turn if user already won
+
+        cross_board(comp_board, chosen_number)
+        comp_score = check_board(comp_board)
+        check_win()
+
+    if dim == None or user_board == []: return True
 
     crossed=[]
     print_boards(user_board, comp_board)
 
-    while state == None:
-        handleInput("Press 'Enter' to choose a random number.. ")
+    while user_won == None:
+        chosen_number = handle_number_validation(dim, crossed, name, user_board, comp_board)
 
-        randomNumber = generateRandomNumber(dim, crossed)
+        if chosen_number == 0:
+            return True
 
-        handleInput(f"Press 'Enter' again to cross '{randomNumber}' out of your board.. ")
-        
-        handleTurns(user_board, comp_board)
+        handleTurns()
 
         print_boards(user_board, comp_board, True)
-    # if state == 1: print("Lucky you won that one!")
-    # else: print("Got ya! I won :) \nGoodluck next time. ")
-    print("Lucky you won that one!" if state == 1 else "Got ya! I won :) \n Goodluck next time. ")
-    bye(name)
+        
+        print(f"Your Progress: {(BINGO[:user_score] + PENDING_WORD[user_score:])}\nComp Progress: {BINGO[:comp_score] + PENDING_WORD[comp_score:]}")
+    else:
+        print(get_random_message("win_scene" if user_won else "loss_scene"))
+        bye()
+        return False
+    
+def save_game(file_name, user_board=[], comp_board=[]):
+    def write_board(board):
+         for row in board:
+            for num in row:
+                output = str(num)
+                if row.index(num) != dim - 1:
+                    output+=' '
+                outFile.write(output)
+            outFile.write("\n")
 
+    not_finished = user_board and comp_board
+    outFile = open(f"{file_name}.txt", "w")
+    
+    outFile.write("Scores:\n")
+    outFile.write(f"{user_score} {comp_score} {user_total_score} {comp_total_score}\n" if not_finished else f"{user_total_score} {comp_total_score}\n")
+
+    if not_finished:
+        outFile.write("User Board:\n")
+        write_board(user_board)
+
+        outFile.write("Computer Board:\n")
+        write_board(comp_board)
+
+    print(f"Game saved successfully as '{file_name}.txt'")
+
+def load_game():
+    pass
+    
 def main(name = None):
-    global dim
-    if name == None:
-        name = input("Hi, What's your name? ")
-        welcome(name)
-        print("And your age? ", end=" ") # end=" " so it stays on the same line, preserving its disguise as the input() function
-        wait(3)
-        print("\nJoking :D ")
+    global dim, user_won, user_score, comp_score
+    
+    user_board, comp_board = [], []
 
-    dim = handle_dims() # dim is the return value of the function handle_dims (always integer) 
-    if not dim: # a flag indicating the exit (user pressed "q") 
-        bye(name)
-        return
+    if name:
+        user_board, comp_board = get_dimension()
+    else:
+        choice = input("1. Start a New Game\n2. Load a Saved Game\n3. Quit\n- ")
+        while choice not in ("1", "2", "3"):
+            choice = input("Invalid input. Enter '1', '2', or '3' :) ")
 
-    user_board, comp_board = init(dim) # destructures the tuple-like return value from init()
+        if choice == "1":
+            name = input(f"{get_random_message('welcome_scene')}, What's your name? ")
+            welcome(name)
+            user_board, comp_board = get_dimension()
+        elif choice == "2":
+            saved_game = load_game()
+            if saved_game: # user didn't quit causing an empty return
+                name, user_board, comp_board = saved_game 
+                welcome(name)
+            else: return
+        else:
+            bye()
+            return
 
-    play(user_board, comp_board, name)
-    handleReset(name)
+    while True:
+        user_quit = play_game(name, user_board, comp_board)
+        if user_quit: break
+
+        print(f"Your Total Score: {user_total_score}\nComp Total Score: {comp_total_score}\n")
+
+        choice = input("Do you want to save the game? (y/n) ")
+        if choice.lower() == "y":
+            save_game(name) # no need to save boards if game already ended
+
+        # Resets all global variables except total score since the game will continue (but with different dimensions, and the score is zero).
+        dim = None
+        user_won = None
+        user_score = 0
+        comp_score = 0
+
+        user_board, comp_board = [], []
+
+        main(name)
 
 # Main Program :>
 main()
--- Bingo Game/utility.py
+++ Bingo Game/utility.py
@@ -1,16 +1,19 @@
-from random import randint, random
-from time import sleep
+from random import randint
 
 def init_arr(dim):
     return list([] for _ in range(dim)) # initializing empty 2d arrays
 
-def generateRandomNumber(dim, crossed):
-    randomNumber = randint(1, dim**2)
-    while randomNumber in crossed:
-        randomNumber = randint(1, dim**2)
+# Didn't find any use cases for the dictionary so I implemeneted a random messenger :D
+def get_random_message(key):
+    random_messages = {
+        "welcome_scene": ["Hello", "Hi", "Welcome", "Greetings", "Nice to see you"],
+        "game_scene": ["You won't win, trust me", "Think twice", "Oh I see what you're doing..", "That won't help still..", "The challenge is on", "Ready to test your skills?"],
+        "bye_scene": ["Thank you for playing! See you later", "Was a good game, GGs", "Fabulous! See you as soon as possible", "Goodbye!"],
+        "win_scene": ["Lucky you won that one.", "You won.. Just don't get used to it.", "Next time you won't get a single point!", "Victory is sweet", "Impressive win!"],
+        "loss_scene": ["Easy, good luck next time.", "Wasn't that hard to beat you", "Don't you think you should be practicing more by now?", "Oops, I won", "Better luck next time!"],
+    }
 
-    crossed.append(randomNumber)
-    return randomNumber
-
-def wait(multiplier = 2):
-    sleep(random() * multiplier)
+    try:
+        return random_messages[key][randint(0, len(random_messages[key]) - 1)]
+    except KeyError:
+        return f"No messages found for key: {key}"
