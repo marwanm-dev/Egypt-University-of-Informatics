1de5ee3b6eb7a526213bf5cd80722603e22c0bc5
marwanm-dev <marwanm.dev@gmail.com>
2024-05-17 20:54:14 +0300

    feat: finished Course class

--- Student-Management-System/include/Course.h
+++ Student-Management-System/include/Course.h
@@ -1,1 +1,35 @@
-class Course {};
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <iostream>
+using namespace std;
+
+class Course {
+  string name;
+  string code;
+  int *instructorIds;
+  int credits;
+  int *studentIds;
+  double *studentGrades;
+  int numStudents;
+  int numInstructors;
+  const int MAX_INSTRUCTORS;
+
+public:
+  Course();
+  Course(string, string, int);
+  ~Course();
+  void addInstructorId(int);
+  void addStudentId(int);
+  double getMaxGrade();
+  double getMinGrade();
+  double getAvgGrade(int);
+  string getCode() { return code; }
+  int getNumStudents() { return numStudents; };
+  int *getStudentIds() { return studentIds; };
+  double *getStudentGrades() { return studentGrades; };
+  void incrementNumStudents();
+  // Add overloaded "=" operator
+};
+
+#endif
--- Student-Management-System/src/Course.cpp
+++ Student-Management-System/src/Course.cpp
@@ -1,4 +1,58 @@
 #include "../include/Course.h"
-#include <iostream>
 
-using namespace std;
+Course::Course()
+    : name(""), code(0), numInstructors(0), credits(0), studentIds(0),
+      studentGrades(0), numStudents(0), instructorIds(0), MAX_INSTRUCTORS(3) {}
+Course::Course(string name, string code, int credits)
+    : numStudents(0), numInstructors(0), MAX_INSTRUCTORS(3) {
+  this->name = name;
+  this->code = code;
+  this->credits = credits;
+  instructorIds = new int[MAX_INSTRUCTORS];
+  studentIds = new int[1];
+  studentGrades = new double[1];
+}
+Course::~Course() {
+  delete[] instructorIds;
+  delete[] studentIds;
+  delete[] studentGrades;
+}
+
+void Course::addInstructorId(int id) { instructorIds[numInstructors++] = id; }
+void Course::addStudentId(int id) {
+  studentIds[numStudents] = id;
+  studentGrades[numStudents++] = id;
+}
+double Course::getMaxGrade() {
+  int max = studentGrades[0];
+  for (int i = 1; i < numStudents; i++) {
+    if (studentGrades[i] > max) {
+      max = studentGrades[i];
+    }
+  }
+  return max;
+}
+double Course::getMinGrade() {
+  int min = studentGrades[0];
+  for (int i = 0; i < numStudents; i++) {
+    if (studentGrades[i] < min) {
+      min = studentGrades[i];
+    }
+  }
+  return min;
+}
+double Course::getAvgGrade(int id = -1) {
+  double sum = 0;
+  if (id == -1) {
+    for (int i = 0; i < numStudents; i++) {
+      sum += studentGrades[i];
+    }
+    return sum / numStudents;
+  } else {
+    for (int i = 0; i < numStudents; i++) {
+      if (studentIds[i] == id) {
+        return studentGrades[i];
+      }
+    }
+  }
+}
