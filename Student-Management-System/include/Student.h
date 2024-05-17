e8a7fa02e7d83db46c41c2d9db468ff53244790e
marwanm-dev <marwanm.dev@gmail.com>
2024-05-17 20:55:41 +0300

    feat: finished Student class

--- Student-Management-System/include/Student.h
+++ Student-Management-System/include/Student.h
@@ -1,1 +1,26 @@
-class Student {};
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "../include/Course.h"
+#include "../include/User.h"
+#include <iostream>
+using namespace std;
+
+class Student : public User {
+  Course *courses;
+  double *grades;
+  int numCourses;
+  const int MAX_COURSES;
+
+public:
+  Student();
+  Student(string, string);
+  void registerCourse(Course);
+  void dropCourses(string);
+  double getGrade(string);
+  double getAvgGrades();
+  void display();
+  // add the overloaded "=" operator
+};
+
+#endif
--- Student-Management-System/src/Student.cpp
+++ Student-Management-System/src/Student.cpp
@@ -1,4 +1,43 @@
 #include "../include/Student.h"
-#include <iostream>
+#include "../include/Course.h"
 
-using namespace std;
+Student::Student() : numCourses(0), MAX_COURSES(6) {}
+Student::Student(string username, string password)
+    : User(username, password), numCourses(0), MAX_COURSES(6) {
+  courses = new Course[MAX_COURSES];
+  grades = new double[MAX_COURSES];
+}
+
+void Student::registerCourse(Course course) {
+  courses[numCourses++] = course;
+  if (numCourses >= MAX_COURSES)
+    cout << "can't register more";
+}
+
+void Student::dropCourses(string code) {
+  for (int i = 0; i < numCourses; ++i) {
+    if (courses[i].getCode() == code) {
+      Course temp = courses[i];
+      courses[i] = courses[numCourses - 1];
+      courses[numCourses - 1] = temp;
+      numCourses--;
+    }
+  }
+}
+double Student::getGrade(string code) {
+  for (int i = 0; i < numCourses; ++i) {
+    if (courses[i].getCode() == code) {
+      int numStudents = courses[i].getNumStudents();
+      for (int j = 0; j < numStudents; ++j) {
+        if (courses[i].getStudentIds()[j] == getId()) {
+          return courses[i].getStudentIds()[j];
+        }
+      }
+    }
+  }
+}
+void Student::display() {
+  User::display();
+  cout << "Role: student" << endl;
+  cout << courses[0].getCode();
+}
