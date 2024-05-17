01ec9a44ca882667d7715da0555ccbc5cddcc883
marwanm-dev <marwanm.dev@gmail.com>
2024-05-17 20:54:38 +0300

    feat: finished Instructor class

--- Student-Management-System/include/Instructor.h
+++ Student-Management-System/include/Instructor.h
@@ -1,1 +1,25 @@
-class Instructor {};
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+
+#include "Course.h"
+#include "User.h"
+#include <iostream>
+using namespace std;
+
+class Instructor : public User {
+private:
+  Course *courses;
+  int numCourses;
+  const int MAX_COURSES;
+
+public:
+  Instructor();
+  Instructor(string, string);
+  void addCourse(Course);
+  void removeCourse(string);
+  void setGrade(string, int, double);
+  double performStats(string, string);
+  // add the overloaded "=" operator
+};
+
+#endif
--- Student-Management-System/src/Instructor.cpp
+++ Student-Management-System/src/Instructor.cpp
@@ -1,4 +1,52 @@
 #include "../include/Instructor.h"
-#include <iostream>
+#include "../include/Course.h"
+#include "../include/User.h"
+#include <string>
 
-using namespace std;
+Instructor ::Instructor(string username, string password)
+    : User(username, password), MAX_COURSES(5) {
+  numCourses = 0;
+  courses = new Course[MAX_COURSES];
+}
+void Instructor ::addCourse(Course course) { courses[numCourses++] = course; }
+void Instructor ::removeCourse(string code) {
+  for (int i = 0; i < numCourses; i++) {
+    if (courses[i].getCode() == code) {
+      Course temp = courses[i];
+      courses[i] = courses[numCourses - 1];
+      courses[numCourses - 1] = temp;
+      numCourses--;
+    }
+  }
+}
+void Instructor ::setGrade(string code, int id, double grade) {
+  for (int i = 0; i < numCourses; i++) {
+    if (courses[i].getCode() == code) {
+      int numStudents = courses[i].getNumStudents();
+      for (int j = 0; j < numStudents; j++) {
+        if (courses[i].getStudentIds()[j] == id) {
+          courses[i].getStudentGrades()[j] = grade;
+        } else {
+          courses[i].getStudentIds()[numStudents] = id;
+          courses[i].getStudentGrades()[numStudents] = grade;
+          courses[i].incrementNumStudents();
+        }
+      }
+    }
+  }
+}
+double Instructor ::performStats(string code, string type) {
+  for (int i = 0; i < numCourses; i++) {
+    if (courses[i].getCode() == code) {
+      if (type == "max") {
+        courses[i].getMaxGrade();
+      }
+      if (type == "min") {
+        courses[i].getMinGrade();
+      }
+      if (type == "avg") {
+        courses[i].getAvgGrade(-1);
+      }
+    }
+  }
+};
