d0db31b9b70c651459b11d5db245a0d6bc39461a
marwanm-dev <marwanm.dev@gmail.com>
2024-05-17 20:53:47 +0300

    feat: finished Administrator class

--- Student-Management-System/include/Administrator.h
+++ Student-Management-System/include/Administrator.h
@@ -1,1 +1,29 @@
-class Administrator {};
+#ifndef ADMINISTRATOR_H
+#define ADMINISTRATOR_H
+
+#include "../include/Course.h"
+#include "../include/Instructor.h"
+#include "../include/Student.h"
+#include "../include/User.h"
+#include <iostream>
+using namespace std;
+
+class Administrator : public User {
+  Instructor *instructors;
+  Student *students;
+  Course *courses;
+  int numInstructors;
+  int numStudents;
+  int numCourses;
+
+public:
+  Administrator(const string &, const string &);
+  void addStudent(const Student &);
+  void removeStudent(const int &);
+  void addInstuctor(const Instructor &);
+  void removeInstructor(const int &);
+  void addCourse(const Course &);
+  void removeCourse(const string &);
+};
+
+#endif
--- Student-Management-System/src/Administrator.cpp
+++ Student-Management-System/src/Administrator.cpp
@@ -1,4 +1,42 @@
 #include "../include/Administrator.h"
-#include <iostream>
+#include "../include/Instructor.h"
+#include "../include/User.h"
+#include "swapIndices.cpp"
 
-using namespace std;
+Administrator::Administrator(const string &username, const string &password)
+    : User(username, password), numInstructors(0), numStudents(0),
+      numCourses(0), instructors(new Instructor[1]), students(new Student[1]),
+      courses(new Course[1]) {}
+void Administrator::addStudent(const Student &student) {
+  // students[numStudents++] = student;
+}
+void Administrator::removeStudent(const int &id) {
+  for (int i = 0; i < numStudents; ++i) {
+    if (students[i].getId() == id) {
+      // swapIndices(students[i], students[numStudents - 1]);
+      numStudents--;
+    }
+  }
+}
+void Administrator::addInstuctor(const Instructor &instructor) {
+  // instructors[numInstructors++] = instructor;
+}
+void Administrator::removeInstructor(const int &id) {
+  for (int i = 0; i < numInstructors; i++) {
+    if (instructors[i].getId() == id) {
+      // swapIndices(instructors[i], instructors[numInstructors - 1]);
+      numInstructors--;
+    }
+  }
+}
+void Administrator::addCourse(const Course &course) {
+  // courses[numCourses++] = course;
+}
+void Administrator::removeCourse(const string &code) {
+  for (int i = 0; i < numCourses; i++) {
+    if (courses[i].getCode() == code) {
+      // swapIndices(courses[i], courses[numCourses - 1]);
+      numCourses--;
+    }
+  }
+}
