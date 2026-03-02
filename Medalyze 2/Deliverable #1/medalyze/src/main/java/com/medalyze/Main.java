58b076b7a03f05e40944a2de66dde349cad13ea3
Marwan Mostafa <marwanm.dev@gmail.com>
2026-03-02 22:41:25 +0200

    initial commit

---
+++ Medalyze 2/Deliverable #1/medalyze/src/main/java/com/medalyze/Main.java
@@ -0,0 +1,29 @@
+package com.medalyze;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Patient patient = new Patient("P1", "Marwan", "0100000000");
+        Doctor doctor = new Doctor("D1", "Cardiology");
+
+        Appointment appointment = new Appointment(
+                "A1",
+                LocalDate.now(),
+                LocalTime.now(),
+                "Scheduled",
+                patient,
+                doctor
+        );
+
+        appointment.cancel();
+
+        Report report = new Report("R1", "Billing");
+        report.generate();
+
+        System.out.println("System running successfully.");
+    }
+}
